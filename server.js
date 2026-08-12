2fa0d90ddac61642fdb2eb7815883c8c583e9e0f
socialboosthub <muxidinxamza@gmail.com>
2026-08-12 17:56:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 3000;
 
 const TIKTOK_USERNAME =
-  process.env.TIKTOK_USERNAME || "yaboysavage_";
+  process.env.TIKTOK_USERNAME || "lxkt16";
 
 // ==========================================
 // EXPRESS
